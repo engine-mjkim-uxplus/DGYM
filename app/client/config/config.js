f63624d280982777c17f909313416d136d0cfcb9
odong2 <mjey54@naver.com>
2022-08-27 00:27:32 +0900

    DGYM프로젝트 배포 완료

---
+++ app/client/config/config.js
@@ -0,0 +1,14 @@
+// 본인의 key를 입력해 주세요
+// const firebaseConfig = {
+//   apiKey:
+//   authDomain:
+//   databaseURL:
+
+//   projectId:
+//   storageBucket:
+//   messagingSenderId:
+//   appId:
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
